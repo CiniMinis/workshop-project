9514504a0d0a73d36aeb4a938d195804efae5cea
Jonathan Kalish <33545226+CiniMinis@users.noreply.github.com>
2021-08-04 17:59:59 +0300

    Updated app creation
    Added instance-independednt app creation

--- app/__init__.py
+++ app/__init__.py
@@ -9,10 +9,10 @@ from app.views import views
 db = SQLAlchemy()
 config_factory = AppConfigFactory()
 
-def create_app():
+def create_app(config_type=app_config_type):
     app = Flask(__name__)
     # configuration for development
-    app_config = config_factory.make(app_config_type)
+    app_config = config_factory.make(config_type)
     app.config.from_object(app_config)
     app.register_blueprint(api)
     app.register_blueprint(views)
