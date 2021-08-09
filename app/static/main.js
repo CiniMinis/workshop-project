a3661f0cfa6456332fb71f4dc7fbb5151bb75efe
Jonathan Kalish <33545226+CiniMinis@users.noreply.github.com>
2021-08-09 12:41:48 +0300

    Remade Navbar Scripts and Layout
    Tweaked order of items in navbar and generalized the functionality of icon changes on collapse updates.

--- app/static/main.js
+++ app/static/main.js
@@ -153,3 +153,23 @@ function draw_all_avatars(){
 }
 
 $().ready(draw_all_avatars);
+
+
+function bind_button_to_collapse(button_id, collapsable_id, extend_icon="bi-plus-lg", retract_icon="bi-dash-lg") {
+    // make icon show retraction when extended
+    $(`#${collapsable_id}`).on('show.bs.collapse', () => {
+        $(`#${button_id} > i`).addClass(retract_icon).removeClass(extend_icon);
+    });
+
+    // make icon show extention when retracted
+    $(`#${collapsable_id}`).on('hide.bs.collapse', () => {
+        $(`#${button_id} > i`).addClass(extend_icon).removeClass(retract_icon);
+    });
+
+    // set current icon to match starting state
+    if ($(`#${collapsable_id}`).attr("aria-expanded")) {
+        $(`#${button_id} > i`).addClass(retract_icon).removeClass(extend_icon);
+    } else {
+        $(`#${button_id} > i`).addClass(extend_icon).removeClass(retract_icon);
+    }
+}
