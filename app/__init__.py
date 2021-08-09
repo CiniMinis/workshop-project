2485fcb46bc16d24de73d9f3121f2c36b5d3c76a
Jonathan Kalish <33545226+CiniMinis@users.noreply.github.com>
2021-08-09 17:22:08 +0300

    Enable Session Type Configurations
    Added ability to make user sessions save on server-side. Specifically added SQL session mode.

--- app/__init__.py
+++ app/__init__.py
@@ -1,24 +1,25 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from instance import app_config_type
+from instance import app_config_type, session_config_type
 from app.config import AppConfigFactory
 
 db = SQLAlchemy()
 config_factory = AppConfigFactory()
 
-def create_app(config_type=app_config_type):
+def create_app(app_type=app_config_type, session_type=session_config_type):
     app = Flask(__name__)
     # configuration for development
-    app_config = config_factory.make(config_type)
+    app_config = config_factory.make(app_type, session_type)
     app.config.from_object(app_config)
 
     # imports inside create_app to avoid circular import
     from app.api import api
     from app.controllers import controllers
-    
+
     app.register_blueprint(api)
     app.register_blueprint(controllers)
 
     db.init_app(app)
+    app_config.init_session(app)
 
     return app
--- app/config/__init__.py
+++ app/config/__init__.py
@@ -1,7 +1,6 @@
 import os
 from abc import ABC, abstractmethod
-
-from flask import config
+from flask_sessionstore import Session
 from instance import session_key
 
 FILE_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -10,7 +9,6 @@ INSTANCE_DIR = os.path.join(FILE_DIR, '../../instance')
 # general config consts
 class AppConfig(ABC):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = session_key
 
     @property
     @abstractmethod
@@ -34,14 +32,46 @@ class AppTestConfig(AppConfig):
 
 # TODO: make configurations for production
 
-class AppConfigFactory:
-    DEV_NAMES = ['dev', 'development']  # names for development
-    TEST_NAMES = ['test', 'testing']
 
-    def make(self, config_type):
-        if config_type in self.DEV_NAMES:
-            return AppDevConfig()
-        if config_type in self.TEST_NAMES:
-            return AppTestConfig()
+# configuration for default flask sessions
+class SessionConfig:
+    SECRET_KEY = session_key
+    SESSION_USE_SIGNER = True   # for sessionstore child classes
+
+    def init_session(self, app):
+        pass
+
+# configuration for sqlalchemy flask session
+class SQLSessionConfig(SessionConfig):
+    SESSION_TYPE = 'sqlalchemy'
+
+    def init_session(self, app):
+        session = Session(app)
+        session.app.session_interface.db.create_all()
+
+class AppConfigFactory:
+    DEV_CONFIG_NAMES = ['dev', 'development']  # names for development
+    TEST_CONFIG_NAMES = ['test', 'testing']
+
+    DEFAULT_SESSION_NAME = [None, 'default', 'cookie']
+    SQL_SESSION_NAME = ['sql', 'sqlalchemy']
+
+    def make(self, config_type, session_type=None):
+        if config_type in self.DEV_CONFIG_NAMES:
+            app_config = AppDevConfig
+        elif config_type in self.TEST_CONFIG_NAMES:
+            app_config = AppTestConfig
+        else:
+            raise ValueError("Invalid Config Type")
         
-        raise ValueError("Invalid Config Type")
+        if session_type in self.DEFAULT_SESSION_NAME:
+            session_config = SessionConfig
+        elif session_type in self.SQL_SESSION_NAME:
+            session_config = SQLSessionConfig
+        else:
+            raise ValueError("Invalid Session Type")
+
+        class MyConfig(app_config, session_config):
+            pass
+
+        return MyConfig()
